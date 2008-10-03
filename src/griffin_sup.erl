53fb3786b8e4db0a81cfc504b7fbf28216bb77f0
Tim Potter <tpot@hp.com>
2008-10-03 11:18:13 +1000

    Whitespace.

--- src/griffin_sup.erl
+++ src/griffin_sup.erl
@@ -11,6 +11,8 @@ start_link() ->
     supervisor:start_link({local, ?SERVER}, ?MODULE, []).
 
 init([]) ->
+
     CIMXMLServer = {cimxml_server, {cimxml_server, start_link, []},
                     permanent, 2000, worker, [cimxml_server]},
+
     {ok, {{one_for_one, 5, 10}, [CIMXMLServer]}}.
