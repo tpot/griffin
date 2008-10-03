1d286eeefc6c94e15c199cf263b6cd23e7bb084e
Tim Potter <tpot@hp.com>
2008-10-03 21:25:39 +1000

    Add dummy handler for HTTP POST operation.

--- src/mod_wbem.erl
+++ src/mod_wbem.erl
@@ -4,8 +4,19 @@
 
 -include_lib("inets/src/httpd.hrl").
 
+do_request(_Headers, _Body) ->
+    {400, [{"CIMError", "request-not-valid"}], ""}.    
+
 do(Info) ->
     case Info#mod.method of
+        "POST" ->
+            {Status, Headers, Body} = do_request(Info#mod.parsed_header,
+                                                 Info#mod.entity_body),
+            Length = integer_to_list(httpd_util:flatlength(Body)),
+            Head = [{code, Status},
+                    {content_type, "application/xml"},
+                    {content_length, Length}] ++ Headers,
+            {proceed, [{response, {response, Head, Body}}]};
         _ ->
             {proceed, [{status, {404, Info#mod.request_uri, ""}}]}
     end.
