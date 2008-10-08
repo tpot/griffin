bdea4f3a28cb46f203f8ad50f95704337ec09b35
Tim Potter <tpot@hp.com>
2008-10-08 13:18:28 +1100

    Minor refactoring of function names.

--- src/mod_wbem.erl
+++ src/mod_wbem.erl
@@ -4,14 +4,46 @@
 
 -include_lib("inets/src/httpd.hrl").
 
-do_request(_Headers, _Body) ->
-    {400, [{"CIMError", "request-not-valid"}], ""}.    
+%% Parsing and validation of request
+
+cimxml_request_not_well_formed() ->
+    {400, [{"CIMError", "request-not-well-formed"}], ""}.
+
+cimxml_request_not_valid() ->
+    {400, [{"CIMError", "request-not-valid"}], ""}.
+
+cimxml_request(Doc) ->
+    case (catch cimxml_parse:parse_xml(Doc)) of
+        {'EXIT', _Reason} ->
+            %% TODO: return error reason in HTTP header
+            cimxml_request_not_well_formed();
+        {error, {_ErrorType, _Description}} ->
+            %% TODO: return error reason in HTTP header
+            cimxml_request_not_valid();
+        RequestTT ->
+            cixml_request_not_valid()
+    end.
+
+%% Return a tuple of {Status, Headers, Body} for the incoming request
+%% headers and XML body.
+
+do(_Headers, Body) ->
+    case (catch xmerl_scan:string(Body, [{quiet, true}])) of
+        {'EXIT', {fatal, _XmlError}} ->
+            cimxml_request_not_well_formed();
+        {'EXIT', _XmlError} ->
+            cimxml_request_not_well_formed();
+        {Doc, _Trailer} ->
+            cimxml_request(Doc)
+    end.
+
+%% Entry point for inets module
 
 do(Info) ->
     case Info#mod.method of
         "POST" ->
-            {Status, Headers, Body} = do_request(Info#mod.parsed_header,
-                                                 Info#mod.entity_body),
+            {tatus, Headers, Body} = 
+                do(Info#mod.parsed_header, Info#mod.entity_body),
             Length = integer_to_list(httpd_util:flatlength(Body)),
             Head = [{code, Status},
                     {content_type, "application/xml"},
