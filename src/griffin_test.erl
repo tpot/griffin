fb461e8631251777937d0a1fdd87e4345a8beaa5
Tim Potter <tpot@hp.com>
2008-10-20 15:27:20 +1100

    Split up mod_wbem into CIM-XML and WS-Man components.

--- src/griffin_test.erl
+++ src/griffin_test.erl
@@ -2,6 +2,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 griffin_test_() ->
-    [{module, mod_wbem_test},
+    [{module, mod_wbem_cimxml_test},
+     {module, mod_wbem_wsman_test},
      {module, cimxml_parse_test},
      {module, cimomhandle_test}].
