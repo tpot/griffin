41f0baee00efff2433a2b41f66b443603ef26a6e
Tim Potter <tpot@hp.com>
2008-11-06 13:33:08 +1100

    Add startup function.

--- src/griffin_app.erl
+++ src/griffin_app.erl
@@ -6,10 +6,15 @@
 start(_Type, _StartArgs) ->
     case griffin_sup:start_link() of
         {ok, Pid} ->
+            init(),
             {ok, Pid};
         Error ->
             Error
     end.
 
 stop(_State) ->
+    ok.
+
+init() ->
+    %% Startup stuff here
     ok.
