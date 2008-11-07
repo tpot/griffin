ac9bfbfa47202a97aabecf431d33793bd10c8519
Tim Potter <tpot@hp.com>
2008-11-07 14:45:07 +1100

    Add a wsman header with some action URI's.

---
+++ src/wsman.hrl
@@ -0,0 +1,16 @@
+%% Action URIs
+
+-define(ActionGet, 'http://schemas.xmlsoap.org/ws/2004/09/transfer/Get').
+-define(ActionPut, 'http://schemas.xmlsoap.org/ws/2004/09/transfer/Put').
+-define(ActionCreate, 'http://schemas.xmlsoap.org/ws/2004/09/transfer/Create').
+-define(ActionDelete, 'http://schemas.xmlsoap.org/ws/2004/09/transfer/Delete').
+-define(ActionEnumerate, 
+        'http://schemas.xmlsoap.org/ws/2004/09/enumeration/Enumerate').
+-define(ActionPull, 'http://schemas.xmlsoap.org/ws/2004/09/enumeration/Pull').
+-define(ActionRenew, 'http://schemas.xmlsoap.org/ws/2004/09/enumeration/Renew').
+-define(ActionGetStatus, 
+        'http://schemas.xmlsoap.org/ws/2004/09/enumeration/GetStatus').
+-define(ActionRelease, 
+        'http://schemas.xmlsoap.org/ws/2004/09/enumeration/Release').
+-define(ActionSubscribe,
+        'http://schemas.xmlsoap.org/ws/2004/08/eventing/Subscribe').
