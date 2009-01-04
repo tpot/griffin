6a739f2228f36fda790086d00998979b9f5e6a47
Tim Potter <tpot@hp.com>
2009-01-04 12:08:09 +1100

    Re-enable old tests.

--- src/griffin_tests.erl
+++ src/griffin_tests.erl
@@ -2,7 +2,8 @@
 -include_lib("eunit/include/eunit.hrl").
 
 griffin_test_() ->
-    [{module, mod_wbem_cimxml},
+    [{module, repository},
+     {module, mod_wbem_cimxml},
      {module, mod_wbem_wsman},
      {module, cimxml_parse},
      {module, cimomhandle}].
